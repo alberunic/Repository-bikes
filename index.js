4b2aafad9bc0097077b1e0002b913232114c67d8
alberunic <albertoprs3@outlook.com>
2024-06-22 19:01:35 +0200

    added variable

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-alert("Bienvenido")
+if(window.location.pathname === '/') {
+    alert("Bienvenido") 
 
-const routesButton=document.getElementById("bikes-routes")
-routesButton.addEventListener("click",function () {
-    alert("Te vamos a redireccionar a una ruta")
-})
+    const routesButton=document.getElementById("bikes-routes")
+    routesButton.addEventListener("click",function () {
+        alert("Te vamos a redireccionar a una ruta")
+    })
+}
+
+
+const hour=document.getElementById("hour")
+const localHour= new Date() 
+console.log(localHour)
+hour.innerHTML=localHour
