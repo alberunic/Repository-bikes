c4eb5d6970716638fc1604bdc07fae84d0737a4b
alberunic <albertoprs3@outlook.com>
2024-06-22 19:25:25 +0200

    Added counter

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ if(window.location.pathname === '/') {
 }
 
 
-const hour=document.getElementById("hour")
-const localHour= new Date() 
-console.log(localHour)
-hour.innerHTML=localHour
+console.log(window.location.pathname)
+
+if(window.location.pathname === "/subpage.html") {
+    const hour=document.getElementById("hour")
+    const localHour= new Date() 
+    console.log(localHour)
+    hour.innerHTML=localHour
+    
+    
+    const addButton=document.getElementById("add")
+    const counter=document.getElementById("counter")
+    addButton.addEventListener("click",function () {
+        counter.innerHTML=Number(counter.innerHTML)+1
+    })  
+}
