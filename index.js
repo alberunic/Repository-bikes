81c5e4e5799b5e26f1b92b23c6b61dc66a50f996
alberunic <albertoprs3@outlook.com>
2024-06-22 18:41:05 +0200

    added javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+alert("Bienvenido")
+
+const routesButton=document.getElementById("bikes-routes")
+routesButton.addEventListener("click",function () {
+    alert("Te vamos a redireccionar a una ruta")
+})
